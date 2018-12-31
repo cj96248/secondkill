99201ae26726296c847445006d96278be2a85bae
cj96248 <cj96248@sina.com>
2018-12-31 20:56:01 +0800

    统一验证实现

--- src/main/java/com/imooc/validator/ValidationResult.java
+++ src/main/java/com/imooc/validator/ValidationResult.java
@@ -1,4 +1,34 @@
 package com.imooc.validator;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ValidationResult {
+
+    private boolean hasErrors = false;
+
+    private Map<String, String> errorMessageMap = new HashMap<>();
+
+    public boolean isHasErrors() {
+        return hasErrors;
+    }
+
+    public void setHasErrors(boolean hasErrors) {
+        this.hasErrors = hasErrors;
+    }
+
+    public Map<String, String> getErrorMessageMap() {
+        return errorMessageMap;
+    }
+
+    public void setErrorMessageMap(Map<String, String> errorMessageMap) {
+        this.errorMessageMap = errorMessageMap;
+    }
+
+    public String getErrorMessage(){
+        return StringUtils.join(errorMessageMap.values().toArray(),",");
+    }
+
 }
--- src/main/java/com/imooc/validator/ValidatorImpl.java
+++ src/main/java/com/imooc/validator/ValidatorImpl.java
@@ -1,4 +1,35 @@
 package com.imooc.validator;
 
-public class ValidatorImpl {
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Component
+public class ValidatorImpl implements InitializingBean{
+
+    private Validator validator;
+
+    public ValidationResult validate(Object object){
+        ValidationResult result = new ValidationResult();
+        Set<ConstraintViolation<Object>> set = validator.validate(object);
+        if(set.size() > 0){
+            result.setHasErrors(true);
+            set.forEach(violation ->{
+                String errorMessage = violation.getMessage();
+                String name = violation.getPropertyPath().toString();
+                result.getErrorMessageMap().put(name, errorMessage);
+            });
+        }
+        return result;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
 }
